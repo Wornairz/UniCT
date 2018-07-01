00fc8c85557023a647c9ce8da0499fe203b02e6e
Giuseppe Patané <patanegiuseppe7@gmail.com>
2018-07-01 21:21:01 +0200

    Aggiunti algoritmi di ordinamento
    Creata cartella "algoritmi_ordinamento" ed aggiunti al suo interno un file con gli algoritmi ed un esempio di funzionamento.

---
+++ Prog2/algoritmi_ordinamento/Sorting.h
@@ -0,0 +1,44 @@
+#ifndef SORTING_H
+#define SORTING_H
+
+#include <chrono>
+#include <cstring>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class Sorting
+{
+    private:
+        int lomutoPartition(int*, int, int);
+        int hoarePartition(int*, int, int);
+        void _quickSort(int*, int, int);
+        void _mergeSort(int*, int, int);
+        void merge(int*, int, int, int);
+        void swap(int&, int&);
+    public:
+        Sorting(){srand(time(nullptr));};
+        void insertionSort(int*, int);
+        void selectionSort(int*, int);
+        void bubbleSort(int*, int);
+        void mergeSort(int*, int);
+        void quickSort(int*, int);
+        void printArray(int*, int);
+        int* revSortedArray(int);
+        int* sortedArray(int);
+        int* randomArray(int);
+        bool is_sorted(int*, int);
+        int* all0s(int);
+        void test(int*, int, Sorting&, void (Sorting::*f)(int*, int));
+};
+
+
+
+
+
+
+
+
+
+#endif // SORTING_H
